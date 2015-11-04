69b32170827d8d2793ad0d92f4e4613521e05691
mattwhooo <matt.watson@orasi.com>
2015-11-04 11:19:30 -0500

    Update Basetest

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'hotdog',
   packages = ['hotdog'], # this must be the same as the name above
-  version = '0.1.2',
+  version = '0.1.3',
   description = 'Appium/Selenium testing framework deriving from unittest',
   author = 'Matt Watson',
   author_email = 'Watson.Mattc@gmail.com',
   url = 'https://github.com/Mattwhooo/Hotdog', # use the URL to the github repo
-  download_url = 'https://github.com/Mattwhooo/Hotdog/tarball/0.1.2', # I'll explain this in a second
+  download_url = 'https://github.com/Mattwhooo/Hotdog/tarball/0.1.3', # I'll explain this in a second
   keywords = ['appium', 'selenium', 'testing'], # arbitrary keywords
   classifiers=[],
-  install_requires=['webium', 'testtools','beautifulsoup4','Appium-Python-Client', 'requests', 'sauceclient', 'timeout-decorator']
+  install_requires=['webium', 'testtools','beautifulsoup4','Appium-Python-Client', 'requests', 'sauceclient', 'timeout-decorator', 'appium_selector']
 )
